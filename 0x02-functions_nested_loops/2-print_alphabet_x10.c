777312a108ab3b092f469ab1bbf6a05b2885362f
Mehdi RABBAI <mehdi.rabbai@gmail.com>
2023-02-21 13:56:39 -0800

    add 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+
+}
+
