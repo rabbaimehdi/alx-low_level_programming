bbfc9cf909de883eac8c46648191c56159c8ad31
Mehdi RABBAI <mehdi.rabbai@gmail.com>
2023-02-21 14:47:41 -0800

    add 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	int c;
+	
+	if(c>=65 && c<=90){
+		return 0;
+	}
+	else 
+		return 1;
+}
+
