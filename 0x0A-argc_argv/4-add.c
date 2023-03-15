05b9d82654ef1f1bfcc758db0fd2544adc1b8c9b
Mehdi RABBAI <mehdi.rabbai@gmail.com>
2023-03-15 16:57:01 -0700

    add 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main entry
+ * @argc: arg counter
+ * @argv:  arg values
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num, digit, sum = 0;
+
+	for (num = 1; num < argc; num++)
+	{
+		for (digit = 0; argv[num][digit]; digit++)
+		{
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[num]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
