cd840e995ae0aca8470cdf6e840b8d91e1885bb8
Mehdi RABBAI <mehdi.rabbai@gmail.com>
2023-03-15 13:57:55 -0700

    update statuc kub

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# Compile all .c files in the current directory
 gcc -c *.c
-
-# Create the static library
-ar rcs liball.a *.o
-
-# Clean up object files
-rm *.o
-
+ar rc liball.a *.o
+ranlib liball.a
