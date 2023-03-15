6d9538c2131604042606cd54ad27fef7f0e83406
Mehdi RABBAI <mehdi.rabbai@gmail.com>
2023-03-15 16:15:37 -0700

    add 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ * @argc: arg counter
+ * @argv:  arg values
+ * Return: 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
