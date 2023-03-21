d88d7fcdab3c1659ba92497da0f574b2dd03bfec
Mehdi RABBAI <mehdi.rabbai@gmail.com>
2023-03-21 16:11:23 -0700

    add description for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - A new type for a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+
 struct dog
 {
 	char *name;
