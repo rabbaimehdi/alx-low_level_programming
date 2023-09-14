6cd0d32e43380d8b35e3c3ab47e654563ce429ba
Mehdi <mehdi.rabbai@gmail.com>
2023-09-13 17:24:37 -0700

    correct 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,16 +11,16 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *node;
-	unsigned long int ind;
+	unsigned long int index;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	ind = key_ind((const unsigned char *)key, ht->size);
-	if (ind >= ht->size)
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
 		return (NULL);
 
-	node = ht->array[ind];
+	node = ht->array[index];
 	while (node && strcmp(node->key, key) != 0)
 		node = node->next;
 
